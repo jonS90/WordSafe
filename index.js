b2e24632b01709348b82eff6077a6ac9debf9224
Jon Smithers <mail@jonsmithers.link>
2017-09-14 20:29:09 -0400

    some setup with yargs

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const argv     = require('yargs').argv;
+#!/usr/local/bin/node
 const inquirer = require('inquirer');
+
+require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('init <file>', 'Set up a new encrypted text file',
+      yargs => yargs.option('specificToInit', {
+        describe: 'this option is specific to init'
+      }).example('$0 init my_journal', 'Creates encrypted file my_journal'),
+      argv => {
+        console.log("this is where we handle it", argv.file);
+      }
+    )
+    .option('v', {
+      alias: 'verbose',
+      describe: 'Chatty logs'
+    })
+    .boolean('verbose')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('Copyright 2017')
+    .argv;
+// require('yargs')
+// .command('init', 'setup new encrypted text')
 
 setInterval(() => {}, 1000)
