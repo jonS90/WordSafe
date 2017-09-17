60116933172c52ba0315afb50c164774bcec8f94
Jon Smithers <jsmithe2@gmu.edu>
2017-09-17 16:37:48 -0400

    Re-indent things
    For some reason eslint started complaining

--- wordsafe.js
+++ wordsafe.js
@@ -14,26 +14,26 @@ Description:
 `.slice(1,-1);
 
 require('yargs')
-    .usage(usage)
-    .command(require('./commands/init.js'))
-    .command(require('./commands/edit.js'))
-    .command('push <file>', '(NOT IMPLEMENTED) Open an empty editor for you to type in and then appends its contents to <file>')
-    .command('change-password', '(NOT IMPLEMENTED)')
-    .command('change-cipher', '(NOT IMPLEMENTED)')
-    .command(require('./commands/decrypt.js'))
-    // .command('*', 'wut', ()=>{}, ()=>{ console.error('unrecognized'); })
-    .option('v', {
-      alias: 'verbose',
-      describe: 'Chatty logs'
-    })
-    .boolean('verbose')
-    .help()
-    .command('*', '', ()=>{}, ()=> {
-      // require('yargs').showHelp(); DOESNT WORK FOR DESIRED CONTEXT
-      // re-run wordsafe with "help" command
-      require('child_process').spawn('wordsafe', ['help'], {stdio: 'inherit'});
-    })
-    .example('wordsafe init\nwordsafe edit --editor="vim" <encrypted-file>')
-    // .alias('h', 'help')
-    // .alias('h', '*')
-    .argv;
+  .usage(usage)
+  .command(require('./commands/init.js'))
+  .command(require('./commands/edit.js'))
+  .command('push <file>', '(NOT IMPLEMENTED) Open an empty editor for you to type in and then appends its contents to <file>')
+  .command('change-password', '(NOT IMPLEMENTED)')
+  .command('change-cipher', '(NOT IMPLEMENTED)')
+  .command(require('./commands/decrypt.js'))
+  // .command('*', 'wut', ()=>{}, ()=>{ console.error('unrecognized'); })
+  .option('v', {
+    alias: 'verbose',
+    describe: 'Chatty logs'
+  })
+  .boolean('verbose')
+  .help()
+  .command('*', '', ()=>{}, ()=> {
+    // require('yargs').showHelp(); DOESNT WORK FOR DESIRED CONTEXT
+    // re-run wordsafe with "help" command
+    require('child_process').spawn('wordsafe', ['help'], {stdio: 'inherit'});
+  })
+  .example('wordsafe init\nwordsafe edit --editor="vim" <encrypted-file>')
+  // .alias('h', 'help')
+  // .alias('h', '*')
+  .argv;
