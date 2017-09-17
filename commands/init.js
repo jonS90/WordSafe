96fb6388bab20dc45f7968f455537d4e34501b1b
Jon Smithers <jsmithe2@gmu.edu>
2017-09-17 17:27:09 -0400

    Add streamUtils.fromStrong() util

--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,7 @@
-const encryption = require('../encryption.js');
-const Readable   = require('stream').Readable;
-const fs         = require('fs');
-const yargutils  = require('../utils/yarg-utils.js');
+const encryption  = require('../encryption.js');
+const fs          = require('fs');
+const streamUtils = require('../utils/stream-utils.js');
+const yargutils   = require('../utils/yarg-utils.js');
 
 module.exports = {
   command: 'init [file]',
@@ -21,12 +21,8 @@ module.exports = {
     var password = userinputs.password;
     var file = argv.file || userinputs.file;
 
-    var stringStream = new Readable();
-    stringStream._read = () => {};
-    stringStream.push('Welcome to your encrypted text file :-)\n');
-    //                                                        ^ POSIX
-    stringStream.push(null);
+    var unencryptedStream = streamUtils.fromString('Welcome to your encrypted text file :-)');
 
-    return encryption.streams.encrypt(stringStream, fs.createWriteStream(file), password).catch(console.error);
+    return encryption.streams.encrypt(unencryptedStream, fs.createWriteStream(file), password).catch(console.error);
   }
 };
