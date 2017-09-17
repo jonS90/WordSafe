b6f8d57282e40223c22a5f0ba40dc2b46ab38055
Jon Smithers <jsmithe2@gmu.edu>
2017-09-17 17:15:00 -0400

    Update console-wrapper
    I might trash this

--- console-wrapper.js
+++ console-wrapper.js
@@ -1,11 +1,14 @@
-module.exports = {
-  log(...args) {
-    console.log(...args);
-  },
-  warn(...args) {
-    console.warn(...args);
-  },
-  error(...args) {
-    console.error(...args);
-  }
+module.exports = function getLogger(argv) {
+  return {
+    log(...args) {
+      if (!argv.verbose) return;
+      console.log(...args);
+    },
+    warn(...args) {
+      console.warn(...args);
+    },
+    error(...args) {
+      console.error(...args);
+    }
+  };
 };
